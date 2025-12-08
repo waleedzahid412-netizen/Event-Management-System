0a70c669f31f67fa7e8bed2c8ba33f5b56104ce6
CC <CC@DESKTOP-DBPS5NG>
2025-12-08 05:36:19 -0800

    Add project files.

---
+++ Event Management System/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+using System.Diagnostics;
+using Event_Management_System.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Event_Management_System.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
